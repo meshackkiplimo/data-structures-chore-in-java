8fc7b9668d8cec1181d7285803ee360cdd32ad2e
meshack kimaiyo <meshackkimaiyo5@gmail.com>
2025-01-06 23:40:09 +0300

    array traversal using loops

---
+++ array/ArrayTraversal.java
@@ -0,0 +1,8 @@
+public class ArrayTraversal {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50}; // Define an array of integers
+        for (int i = 0; i < arr.length; i++) { // Loop through the array
+            System.out.println("Element at index " + i + ": " + arr[i]); // Access and print each element
+        }
+    }
+}
