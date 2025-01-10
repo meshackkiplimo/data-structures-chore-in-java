b2df28928509170e8efb0d1bea1a2132181c16ad
meshack kimaiyo <meshackkimaiyo5@gmail.com>
2025-01-10 14:37:49 +0300

    single linked list

---
+++ linkedlist/Singly.java
@@ -0,0 +1,25 @@
+public class Singly{
+
+
+    private ListNode head;
+    public static class ListNode{
+        private int data;
+        private ListNode next;
+
+        public ListNode(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static void main(String[] args) {
+        Singly sll = new Singly();
+        sll.head = new ListNode(10);
+        ListNode second = new ListNode(1);
+        ListNode third = new ListNode(8);
+        ListNode fourth = new ListNode(11);
+        sll.head.next=second;
+        second.next=third;
+        third.next=fourth;
+        // print
+    }
+}
